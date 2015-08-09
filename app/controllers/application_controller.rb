a9f228a686ca7b6ed4c16be36429a251283ace28
Chris <christopherd0000@gmail.com>
2015-08-09 19:07:10 +0000

    Updated Gemfile for deployment to Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello 
-    render text: "<h1>Hello</h1><p>Welcome home</p>"
+    render text: "<h1>Hello</h1><p>Welcome home Chris</p>"
   end
 end
